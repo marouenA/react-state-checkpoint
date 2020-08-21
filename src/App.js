ca10d95d66bf78148c25f2d4de9ce7b0b7d483e7
marouenA <marouen.abdennadher@iit.ens.tn>
2020-08-21 10:47:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class welcome extends  React.Component{
+  constructor(props){
+   super(props);
+  
+  this.state ={
+    person:{fullName:"avatar",
+    bio:"avatar avatar",
+    imgsrc:"https://geeko.lesoir.be/wp-content/uploads/sites/58/2020/05/avatar.jpg",
+    profession:"avatar avatar avatar avatar"},
+    hidden:true,
+    count:0,
+  };
 }
+show=()=>{this.setState({hidden:false})}
+changecount=()=>{this.setState({count:this.state.count+10})}
+componentDidMount () {
+  setInterval(this.changecount,1000)}
 
-export default App;
+  render()
+  {
+  return( 
+    <div>
+
+  {this.state.hidden ?      <button type="button" onClick={this.show}>show person</button>  :    <div> <h1>{this.state.person.fullName}</h1>
+  <h2>{this.state.person.bio}</h2>
+  <h2>{this.state.person.profession}</h2>
+  <h3>{this.state.count}</h3>
+  <img src={this.state.person.imgsrc} alt="avatar" style={{width:"200px"}} />
+  
+   </div>}
+     
+    </div>
+  
+    );
+
+  }
+}
+export default welcome;
+   
